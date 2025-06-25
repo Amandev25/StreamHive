6c5738511198dbe6bb061fc761911f2ad4eeff8c
Amandev25 <amanyadav.dev25@gmail.com>
2025-06-25 23:42:27 +0530

    Added Authentication

--- src/models/user.js
+++ src/models/user.js
@@ -11,7 +11,9 @@
 //   CreatedAt Date
 //   UpdatedAt Date
 // }
-import mongoose from "mongoose";
+import mongoose , {Schema }from "mongoose";
+import bycrypt from "bcrypt";
+import jwt from "jsonwebtoken" ;
 const userSchema = new Schema(
     {
      id : {
@@ -62,5 +64,44 @@ const userSchema = new Schema(
     },
     { timestamps :true}
 )
+userSchema.pre("save" , async function(next){
+    if(!this.isModified("password"))
+    {
+      return next();
+    }
+    this.password = bycrypt.hash(this.password, 10);
+})
+
+userSchema.methods.isPasswordCorrect = async function(password) {
+   return await bycrypt.compare(password, this.password); 
+}
+userSchema.methods.generateAccessToken = function(){
+   // Generate an access token using the user's id and a secret key
+   // Short Lived Access token 
+   return jwt.sign({
+      _id: this.id,
+      email: this.email,
+      username: this.username,
+      fullname:this.fullname
+   },
+    process.env.Refreshtoken 
+   , 
+ {
+  expiresIn: process.env.AccessToken_Expiry // 1 hour expiration
+ });
+}
+userSchema.methods.generateRefreshToken = function(){
+   // Generate an access token using the user's id and a secret key
+   // Short Lived Access token 
+   return jwt.sign({
+      _id: this.id,
+      
+   },
+    process.env.RefreshtokenSecret
+   , 
+ {
+  expiresIn: process.env.RefreshtokenSecret // 1 hour expiration
+ });
+}
 
 export const User = mongoose.model("User", userSchema);
